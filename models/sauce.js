acbba0b94a03443f7888e13a3acadee10da4d418
corentindaval <60287459+corentindaval@users.noreply.github.com>
2020-12-08 07:42:08 +0100

    ajt models

---
+++ models/sauce.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+const { stringify } = require('querystring');
+
+const sauceschema=mongoose.Schema({
+    id:{type:mongoose.Types.ObjectId,require:true},
+    userId:{type:String,require:true},
+    name:{type:String,require:true},
+    manufacturer:{type:String,require:true},
+    description:{type:String,require:true},
+    mainPepper:{type:String,require:true},
+    imageUrl:{type:String,require:true},
+    heat:{type:Number,require:true},
+    likes:{type:Number,require:true},
+    dislikes:{type:Number,require:true},
+    usersLikes:{type:[String],require:true},
+    usersDisliked:{type:[String],require:true}
+});
+
+module.exports=mongoose.model('sauce',sauceschema);
