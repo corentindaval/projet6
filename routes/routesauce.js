9beb22d00028d5b61ba0c5cc86bee2901e763d1a
corentindaval <60287459+corentindaval@users.noreply.github.com>
2020-12-08 07:41:36 +0100

    ajt routes

---
+++ routes/routesauce.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const router=express.Router();
+const sauce=require('../models/sauce');
+const saucectrl=require('../controleurs/ctrlsauce');
+const auth=require('../middleware/auth');
+const multer=require('../middleware/multer-config');
+
+
+router.post('/',auth,multer,saucectrl.postsauce);/*envoie d'une requete post */
+
+/*router.use('/',auth,saucectrl.createsauce); definition du contenu de stuff(format json) */
+
+router.get('/',auth,saucectrl.getsauces);/*recup des sauces */
+
+router.get('/:id',auth,saucectrl.getsauce);/*parametre get */
+
+router.put('/:id',auth,multer,saucectrl.putsauce);/*parametre put */
+
+router.delete('/:id',auth,saucectrl.deletesauce);/*parametre delete */
+
+
+
+module.exports=router;
